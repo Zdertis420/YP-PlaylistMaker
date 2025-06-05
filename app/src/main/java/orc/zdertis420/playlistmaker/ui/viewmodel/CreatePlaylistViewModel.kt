6d77aefc64c27ae3ec7ef9233e91fd1976ac7ebf
Zdertis420 <AnRobo07@yandex.ru>
2025-06-05 20:46:35 +0300

    начал прикручивать интерфейс

--- app/src/main/java/orc/zdertis420/playlistmaker/ui/viewmodel/CreatePlaylistViewModel.kt
+++ app/src/main/java/orc/zdertis420/playlistmaker/ui/viewmodel/CreatePlaylistViewModel.kt
@@ -1,4 +1,26 @@
 package orc.zdertis420.playlistmaker.ui.viewmodel
 
-class CreatePlaylistViewModel {
+import android.net.Uri
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import orc.zdertis420.playlistmaker.domain.usecase.CreatePlaylistUseCase
+import orc.zdertis420.playlistmaker.domain.usecase.SaveImageUseCase
+
+class CreatePlaylistViewModel(
+    private val createPlaylistUseCase: CreatePlaylistUseCase,
+    private val saveImageUseCase: SaveImageUseCase
+) : ViewModel() {
+
+    private val _selectedImageUri = MutableStateFlow<Uri?>(null)
+    val selectedImageUri: StateFlow<Uri?> = _selectedImageUri
+
+    private val _playlistCreationState = MutableStateFlow<Result<Unit>?>(null)
+    val playlistCreationState: StateFlow<Result<Unit>?> = _playlistCreationState
+
+    fun onImageSelected(uri: Uri) {
+        _selectedImageUri.value = uri
+    }
+
+    
 }
